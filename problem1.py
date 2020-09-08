e592d058fb6ac22883cb0b69d6b20f90022249e0
jtalanges <jtt3@illinois.edu>
2020-09-08 18:56:47 +0000

    I added my name, favorite food/color/hobby.

--- problem1.py
+++ problem1.py
@@ -1,7 +1,23 @@
 ### PROBLEM 1
 
 def main():
-    print("hello world!")
-
+    print("Name: J. Thomas Talanges III")
 if __name__ == "__main__":
     main()
+def main():
+    print("Favorite Food: Cheese")
+if __name__ == "__main__":
+    main()
+def main():
+    print("Favorite Color: Green")
+if __name__ == "__main__":
+    main()
+def main():
+    print("Favorite Hobby: Fishing")
+if __name__ == "__main__":
+    main()
+
+#Name: J. Thomas Talanges III
+#Favorite Food: Cheese
+#Favorite Color: Green
+#Favorite Hobby: Fishing
