7df6a381ded4b2789f352bf6e409818e0ef28c54
jtalanges <jtt3@illinois.edu>
2020-09-09 01:16:31 +0000

    I defined the variables in the equation and made an equation to determine celsius or Fahrenheit.

--- problem3.py
+++ problem3.py
@@ -1,7 +1,15 @@
 ### PROBLEM 3
-
 def main():
-    print("hello world!")
-
+    print("degF= Fahrenheit")
 if __name__ == "__main__":
     main()
+def main():
+    print("Answer= Celsius")
+if __name__ == "__main__":
+    main()
+
+Answer = ((degF)-32) / (1.8)
+print(Answer)
+
+#degF= Fahrenheit
+#Answer= Celsius
